052e8d7c1f25e07f9e793b8d628810e64854e9d6
Karla Cervantes <cervantes.karla14@gmail.com>
2020-12-11 12:27:04 -0600

    character cards created and styled

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import Character from "./components/Character"
+import Details from "./components/Details"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [character, setCharacter] = useState([]);
+  const [characterId, setCharacterId] = useState("1");
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const openDetails = (id) =>{
+  setCharacterId(id);
+};
+const closeDetails = () => {
+  setCharacterId(null);
+};
+
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character`)
+    .then((res) => {
+      setCharacter(res.data.results);
+      // console.log(res.data.results[1]);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characterId && (
+        <Details charaId={characterId} close={closeDetails}/>
+      )}
+      {character.map((ch) => {
+        return <Character key={ch.id} info = {ch} action={openDetails}/>
+      })}
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,59 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+export default function Character(props) {
+    
+    const {info, action} = props
+    return(
+      <StyledCharacter>  
+        <div className="info-container">
+            <img src={info.image}/>
+            <div className="info">
+                <h3>{info.name}</h3>
+                <p>is currently {info.status}</p>
+            </div>
+            {/* <button onClick={() => action(info.id)}>See details</button> */}
+        </div>
+    </StyledCharacter>    
+    );
+}
+
+
+
+const StyledCharacter = styled.div`
+
+.info-container{
+    display:flex;
+    align-items:center;
+    background-color:rgba(0,0,0,.5);
+    border:2px white solid;
+    &:hover{
+        background-color:black;
+    }
+}
+img{
+    margin-left:10%;
+    margin-bottom:2%;
+    margin-top:2%;
+    width:20%;
+    display:flex;
+    justify-content:flex-start;
+    border:1px black solid;
+}
+h3{
+    margin-bottom:1px;
+}
+.info{
+    margin-left:10%;
+    display:flex;
+    flex-direction:column;
+    justify-content:flex-start;
+    color:yellow;
+
+}
+p{
+    margin-left:5%;
+    margin-top:.5px;
+}
+`;
